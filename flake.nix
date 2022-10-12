c09f4ea179119e6f6ac2b95379e93d9da29853c1
Matthias Meschede <MMesch@users.noreply.github.com>
2022-10-12 17:41:25 +0200

    add system dependencies to nix derivation

--- flake.nix
+++ flake.nix
@@ -10,25 +10,8 @@
         config = {allowBroken = true;};
         overlays = [ self.overlay ];
       });
-    in
-    {
-      overlay = (final: prev: {
-        thisPackage = final.haskellPackages.callCabal2nixWithOptions "lib" ./. "--hpack" {};
-      });
-      packages = forAllSystems (system: {
-         thisPackage = nixpkgsFor.${system}.thisPackage;
-      });
-      defaultPackage = forAllSystems (system: self.packages.${system}.thisPackage);
-      checks = self.packages;
-      devShell = forAllSystems (system:
-        let haskellPackages = nixpkgsFor.${system}.haskellPackages;
-            nodePackages = nixpkgsFor.${system}.nodePackages;
-        in haskellPackages.shellFor {
-          packages = p: [self.packages.${system}.thisPackage];
-          withHoogle = true;
-          buildInputs = with haskellPackages; [
-            haskell-language-server
-            cabal-install
+      extraBuildInputs = pkgs : with pkgs; [
+            librsvg
             nodePackages.vega-cli
             # the following is waiting on https://github.com/NixOS/nixpkgs/pull/162434
             (nodePackages.vega-lite.override {
@@ -41,10 +24,42 @@
                     done
                   '';})
             nodePackages.mermaid-cli
-            nixpkgsFor.${system}.graphviz
-            nixpkgsFor.${system}.plantuml
-            nixpkgsFor.${system}.svgbob
-          ];
+            graphviz
+            plantuml
+            svgbob
+      ];
+    in
+    {
+      overlay = final: prev: {
+        thisPackage =
+          let
+            mypkg = (final.haskellPackages.callCabal2nixWithOptions "lib" ./. "--hpack" {});
+          in
+          final.haskell.lib.overrideCabal mypkg (old: {
+            extraLibraries = extraBuildInputs final;
+            testFlags = ["-t 1"];
+            });
+        };
+      packages = forAllSystems (system: {
+         thisPackage = nixpkgsFor.${system}.thisPackage;
+      });
+      defaultPackage = forAllSystems (system: self.packages.${system}.thisPackage);
+      apps = forAllSystems (system: {
+        ddgram = {
+          type = "app";
+          program = "${nixpkgsFor.${system}.thisPackage}/bin/ddgram";
+        };
+      });
+      checks = self.packages;
+      devShell = forAllSystems (system:
+        let haskellPackages = nixpkgsFor.${system}.haskellPackages;
+        in haskellPackages.shellFor {
+          packages = p: [self.packages.${system}.thisPackage];
+          withHoogle = true;
+          buildInputs = with haskellPackages; [
+            haskell-language-server
+            cabal-install
+          ] ++ extraBuildInputs (nixpkgsFor.${system});
           shellHook = ''hpack'';
         });
   };
